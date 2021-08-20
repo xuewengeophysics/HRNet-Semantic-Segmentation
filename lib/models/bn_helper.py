f73b2c66b3944eb1ec2ada87971bf5fc69535fe2
xuewengeophysics <xuewenupc2011@126.com>
2021-08-20 17:05:32 +0800

    debug use single GPU and not use syncBN by modified lib/models/bn_helper.py

--- lib/models/bn_helper.py
+++ lib/models/bn_helper.py
@@ -7,5 +7,8 @@ if torch.__version__.startswith('0'):
     BatchNorm2d_class = InPlaceABNSync
     relu_inplace = False
 else:
-    BatchNorm2d_class = BatchNorm2d = torch.nn.SyncBatchNorm
+    ##https://github.com/HRNet/HRNet-Semantic-Segmentation/issues/222
+    ##If you use single GPU (not distributed mode), do not use syncBN
+    # BatchNorm2d_class = BatchNorm2d = torch.nn.SyncBatchNorm
+    BatchNorm2d_class = BatchNorm2d = torch.nn.BatchNorm2d
     relu_inplace = True
